b4d2d57f3b2f5ab97ce210be26a61f8ff9c6f5b1
Jean <demeljean@hotmail.fr>
2018-03-20 14:30:29 +0100

    added calculus class and test

--- scripts/calculus.js
+++ scripts/calculus.js
@@ -1,20 +1,20 @@
 class Calculus {
 
     operation(a,b,operand){
+        if(typeof(a) !== 'number' || typeof(b) !== 'number') {
+            return NaN;
+        }
         switch (operand) {
-            case "+": return a + b;
-                
-                break;
-            case "-": return a - b;
-                
-                break;
-            case "*": return a * b;
-                
-                break;
-            case "/": return a / b;
-                
-                break;
+            case "+": 
+                return a + b;
+            case "-": 
+                return a - b;
+            case "*": 
+                return a * b;
+            case "/": 
+                return a / b;
             default:
+                throw new Error('wrong operand given');
                 break;
         }
     }
--- test/calculus.spec.js
+++ test/calculus.spec.js
@@ -32,6 +32,22 @@ describe('calculus class',function () {
             expect(calculus.operation(20,2,"/")).toBe(10);
             
         });
+
+        it('should return NaN when a or b is NaN', function() {
+            expect(calculus.operation('bloup', 2, '*')).toBeNaN();
+            expect(calculus.operation('bloup', 2, '/')).toBeNaN();
+            expect(calculus.operation('bloup', 2, '-')).toBeNaN();
+            expect(calculus.operation('bloup', 2, '+')).toBeNaN();
+        });
+
+        it('should not crash when dividing by zero', function() {
+            expect(calculus.operation(10, 0, '/')).toBe(Infinity);
+        });
+
+        it('should throw Error when wrong operand', function() {
+            expect(() => calculus.operation(0, 0, 'bloup'))
+            .toThrow(new Error('wrong operand given'));
+        });
     });
     
 })
